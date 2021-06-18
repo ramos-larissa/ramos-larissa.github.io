6ac5b3a74c8237c43c15ffe29c03a56dd71a7aea
Larissa Ramos <devlarissasr@gmail.com>
2021-06-17 23:26:57 -0300

    criando estrutura inicial da pagina

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,17 @@
 import React from "react";
+import { Button, Card  } from '@material-ui/core';
+import "../Header/style.css"
+
+
 
 export default function Header(){
   return(
     <>
-      <h1>Header aqui!</h1>
+      <div id={'cardHeader'}>
+        <Button id={'buttonHeader'}>Trabalhos</Button>
+        <Button id={'buttonHeader'}>Contato</Button>
+        <Button id={'buttonHeader'}>Sobre mim</Button>
+      </div>
     </>
   )
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,30 @@
-import React from "react";
-import Header from "../../components/Header";
+import React from "react"
+import "../Home/style.css"
+import Header from "../../components/Header"
 
 
-export default function Home(){
-  return(
+
+export default function Home() {
+  return (
     <>
       <Header/>
-      <h1> Teste </h1>
-    </>
-  )
-}
+      <div>
+        <div className={'banner-top'}>
+          <img className={'img-perfil'}
+               src={'https://media-exp3.licdn.com/dms/image/C4E03AQGhMiLkf7CN0g/profile-displayphoto-shrink_800_800/0/1623880288531?e=1629331200&v=beta&t=9ov7Lr0qsrhDU8mjNHH8Coon3GPXGm3GUTVBMQ3bXPg'}/>
+        </div>
+        <div className={'banner-top'}>
+          <p style={{ color: "white", fontSize: 50}}> Larissa Ramos </p>
+        </div>
+      </div>
+        <div id={'card'}>
+          <p> Teste </p>
+          <p> Teste </p>
+          <p> Teste </p>
+          <p> Teste </p>
+          <p> Teste </p>
+        </div>
+
+      </>
+      )
+      }
