5e883ac285260522aec835c89360a49abd419418
Larissa Ramos <devlarissasr@gmail.com>
2021-06-17 23:26:36 -0300

    criando o footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../Footer/style.css";
+
+export default function Footer(){
+  return(
+    <>
+      <div className="footer">
+        <center>Rodapé</center>
+      </div>
+    </>
+  )
+}
