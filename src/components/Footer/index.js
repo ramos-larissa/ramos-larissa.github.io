29fbaa23831fc49126f19d7712c885d87bfa6767
Larissa Ramos <devlarissasr@gmail.com>
2021-06-18 00:59:56 -0300

    cards flip

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "../Footer/style.css";
 
-export default function Footer(){
-  return(
+export default function Footer() {
+  return (
     <>
       <div className="footer">
-        <center>Rodapé</center>
+        <img className={"img-footer"} src={"https://image.flaticon.com/icons/png/512/174/174857.png"}/>
+        <img className={"img-footer"}
+             src={"https://image.flaticon.com/icons/png/512/25/25231.png"}/>
+        <img className={"img-footer"}
+             src={"https://www.ifpb.edu.br/relacoes-internacionais/imagens/email.png/@@images/image.png"}/>
       </div>
     </>
   )
